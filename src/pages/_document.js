dea696a94b7b88aa3bcfc6e7adb3f8890f10085a
Garegin <gareginmiqayelyan8@gmail.com>
2026-01-30 20:01:53 +0400

    Google search

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Search Console Verification */}
+        <meta
+          name="google-site-verification"
+          content="lWzFZ8ETkYyCUecO7_ZngB5mE_NDRBMcIfCzpXM6Roo"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
