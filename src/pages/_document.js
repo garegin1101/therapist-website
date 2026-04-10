eca6e53982fb52afa05be3ca028b0bf3bc32430e
Garegin <gareginmiqayelyan8@gmail.com>
2026-04-10 09:29:04 +0400

    Add robots.txt and sitemap.xml for SEO optimization; update _document.js for improved language detection

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,15 +3,16 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps, pathname: ctx.pathname };
+    const pathname = ctx.req?.url?.split("?")[0] || ctx.pathname || "/";
+    return { ...initialProps, pathname };
   }
 
   render() {
     const { pathname } = this.props;
 
     let lang = "en";
-    if (pathname === "/hy" || pathname === "/book-hy") lang = "hy";
-    else if (pathname === "/ru" || pathname === "/book-ru") lang = "ru";
+    if (pathname.startsWith("/hy") || pathname.startsWith("/book-hy")) lang = "hy";
+    else if (pathname.startsWith("/ru") || pathname.startsWith("/book-ru")) lang = "ru";
 
     return (
       <Html lang={lang}>
